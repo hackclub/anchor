073dbf846b37eb7720b52e48c555b872296ddc71
acon <101356372+acornitum@users.noreply.github.com>
2024-11-06 17:27:19 -0500

    update links to work

--- src/App.js
+++ src/App.js
@@ -39,12 +39,12 @@ export default function App() {
           <div className="flex flex-wrap gap-4 justify-center items-center mt-16 ml-2">
             <div className="flex flex-wrap gap-4">
               <p className="text-6xl text-pink-200">1.</p>
-              <p className="max-w-64 md:max-w-[17rem] text-lg">design a vtuber style logo in <a href="figma.com" className="link" target="_blank" rel="noopenner noreferrer">Figma</a> for your <a href="highseas.hackclub.com" className="link" target="_blank" rel="noopenner noreferrer">High Seas</a> project</p>
+              <p className="max-w-64 md:max-w-[17rem] text-lg">design a vtuber style logo in <a href="https://figma.com" className="link" target="_blank" rel="noopenner noreferrer">Figma</a> for your <a href="https://highseas.hackclub.com" className="link" target="_blank" rel="noopenner noreferrer">High Seas</a> project</p>
             </div>
             <img src="/arrows.svg" alt="arrows" className="w-16 mr-6 hidden md:block"/>
             <div className="flex flex-wrap gap-4">
               <p className="text-6xl text-pink-200">2.</p>
-              <p className="max-w-64 md:max-w-56 text-lg">ship your project out to <a href="highseas.hackclub.com" className="link" target="_blank" rel="noopenner noreferrer">High Seas</a> with your art</p>
+              <p className="max-w-64 md:max-w-56 text-lg">ship your project out to <a href="https://highseas.hackclub.com" className="link" target="_blank" rel="noopenner noreferrer">High Seas</a> with your art</p>
             </div>
             <img src="/arrows.svg" alt="arrows" className="w-16 mr-6 hidden md:block"/>
             <div className="flex flex-wrap gap-4">
@@ -57,7 +57,7 @@ export default function App() {
           <div className="mt-12 md:mt-20 grid grid-cols-1 md:grid-cols-2 gap-8 md:mx-24 mx-8 mb-16">
             <div className="border-4 border-[#B0E298] border-dashed p-6">
               <p className="text-2xl">Getting started:</p>
-              <p className="mt-4">For this YSWS, we're using <a href="figma.com" className="link" target="_blank" rel="noopenner noreferrer">Figma</a>! It's used to design websites and vector art of all kind. <span className="opacity-50">it's also the main (and only) vector art app that i use {":')"}</span></p>
+              <p className="mt-4">For this YSWS, we're using <a href="https://figma.com" className="link" target="_blank" rel="noopenner noreferrer">Figma</a>! It's used to design websites and vector art of all kind. <span className="opacity-50">it's also the main (and only) vector art app that i use {":')"}</span></p>
               <p className="mt-2">You can download Figma here: <a href="https://www.figma.com/downloads/" className="link" target="_blank" rel="noopenner noreferrer">https://www.figma.com/downloads/</a></p>
               <p className="mt-2"></p>
               <p className="mt-2">If you want to use the vtuber logo font, you'll also need to install that from <a href="https://www.freefontdownload.org/en/fot-yuruka-std.font" className="link" target="_blank" rel="noopenner noreferrer">here</a> or <a href="https://archive.org/details/Fontworks" className="link">here</a>.</p>
@@ -67,9 +67,9 @@ export default function App() {
             <div className="border-4 border-[#B0E298] border-dashed p-6">
               <p className="text-2xl">Does it need to be for a High Seas project?</p>
               <p className="mt-4">For now, yes - but that might change in the future!</p>
-              <p className="mt-2">In order to submit, make a High Seas account <a href="highseas.hackclub.com" className="link" target="_blank" rel="noopenner noreferrer">here</a>. When you submit the project you made the logo for, set the cover image as the art!</p>
+              <p className="mt-2">In order to submit, make a High Seas account <a href="https://highseas.hackclub.com" className="link" target="_blank" rel="noopenner noreferrer">here</a>. When you submit the project you made the logo for, set the cover image as the art!</p>
               <p className="mt-2">After your project has recieved payout in High Seas, you'll need to submit just one more form to confirm shipping. More on that later!</p>
-              <p className="mt-8">Any more questions? Ask in <a href="https://hackclub.slack.com/archives/C07V5401VMY" className="link" target="_blank" rel="noopenner noreferrer">#anchor</a> on the slack!</p>
+              <p className="mt-8">Any more questions? Ask in <a href="https://hackclub.slack.com/archives/C07PZMBUNDS" className="link" target="_blank" rel="noopenner noreferrer">#high-seas</a> on the slack!</p>
 
             </div>
           </div>
